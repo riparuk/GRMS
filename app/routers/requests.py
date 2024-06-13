0e4163e6e81083cb6a6d7005fd88e05dfafa5bee
ridhoF-1 <ridho321rg@gmail.com>
2024-06-13 13:57:14 +0700

    update request GET endpoint

--- app/routers/requests.py
+++ app/routers/requests.py
@@ -26,44 +26,61 @@ def create_request(request: schemas.RequestCreate, db: Session = Depends(get_db)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-@router.get("/property/{property_id}", response_model=List[schemas.Request])
-def read_requests_by_property(property_id: str, db: Session = Depends(get_db)):  # Ensure property_id is a string
-    requests = crud.get_requests_by_property(db, property_id=property_id)
-    return requests
+@router.post("/{request_id}/images", response_model=List[schemas.Image])
+async def upload_request_images(request_id: int, files: List[UploadFile] = File(...), db: Session = Depends(get_db)):
+    images = []
+    for file in files:
+        if file.content_type not in ["image/jpeg", "image/png"]:
+            raise HTTPException(status_code=400, detail="Invalid file type")
 
-@router.get("/staff/{staff_id}", response_model=List[schemas.Request])
-def read_requests_by_staff(staff_id: int, db: Session = Depends(get_db)):
-    requests = crud.get_requests_by_staff(db, staff_id=staff_id)
-    return requests
+        destination_blob_name = f"request_images/{uuid.uuid4()}.{file.filename.split('.')[-1]}"
+        public_url = upload_to_gcs(file.file, BUCKET_NAME, destination_blob_name, file.content_type)
+        
+        db_image = crud.create_image(db=db, image=schemas.ImageCreate(filename=file.filename, url=public_url))
+        images.append(db_image)
 
-@router.get("/guest/{guest_id}", response_model=List[schemas.Request])
-def read_requests_by_guest(guest_id: int, db: Session = Depends(get_db)):
-    requests = crud.get_requests_by_guest(db, guest_id=guest_id)
-    return requests
-
-@router.get("/", response_model=List[schemas.Request])
-def read_requests(
-    startDate: Optional[datetime] = None, 
-    endDate: Optional[datetime] = None, 
-    guestName: Optional[str] = None,
-    priority: Optional[float] = None,
-    progress: Optional[str] = None,
-    db: Session = Depends(get_db)
-):
-    requests = crud.get_requests_filtered(db, startDate=startDate, endDate=endDate, guestName=guestName, priority=priority, progress=progress)
-    return requests
-
-@router.get("/{request_id}", response_model=schemas.Request)
-def read_request(request_id: int, db: Session = Depends(get_db)):
     db_request = crud.get_request(db=db, request_id=request_id)
     if db_request is None:
         raise HTTPException(status_code=404, detail="Request not found")
+
+    # Pastikan db_request.imageURLs adalah daftar dan tambahkan URL gambar baru
+    current_image_urls = db_request.imageURLs or []
+    new_image_urls = [image.url for image in images]
+    current_image_urls.extend(new_image_urls)
+    db_request.imageURLs = current_image_urls
+
+    db.commit()
+    db.refresh(db_request)
     
-    if db_request.imageURLs:
-        image_details = crud.get_images_by_id(db=db, ids=db_request.imageURLs)
-        db_request.imageURLs = [schemas.ImageInRequest.from_orm(image) for image in image_details]
+    return images
+
+@router.get("/", response_model=List[schemas.Request])
+def read_requests(
+    start_date: Optional[datetime] = None, 
+    end_date: Optional[datetime] = None, 
+    guest_id: Optional[int] = None,
+    property_id: Optional[str] = None,
+    request_id: Optional[int] = None,
+    assign_to: Optional[int] = None,
+    priority: Optional[float] = None,
+    db: Session = Depends(get_db)
+):
+    requests = crud.get_requests_filtered(
+        db,
+        start_date=start_date,
+        end_date=end_date,
+        guest_id=guest_id,
+        property_id=property_id,
+        request_id=request_id,
+        assign_to=assign_to,
+        priority=priority
+    )
+    for request in requests:
+        if request.imageURLs:
+            image_details = crud.get_images_by_id(db=db, ids=request.imageURLs)
+            request.imageURLs = [schemas.ImageInRequest.from_orm(image) for image in image_details]
     
-    return db_request
+    return requests
 
 @router.put("/{request_id}/assignto/{staff_id}", response_model=schemas.Request)
 def update_assign_to(request_id: int, staff_id: int, db: Session = Depends(get_db)):
@@ -83,37 +100,6 @@ def update_completion_step(request_id: int, step: int, db: Session = Depends(get
     if db_request is None:
         raise HTTPException(status_code=404, detail="Request not found")
     return db_request
-
-@router.post("/{request_id}/images", response_model=List[schemas.Image])
-async def upload_request_images(request_id: int, files: List[UploadFile] = File(...), db: Session = Depends(get_db)):
-    images = []
-    for file in files:
-        if file.content_type not in ["image/jpeg", "image/png"]:
-            raise HTTPException(status_code=400, detail="Invalid file type")
-
-        destination_blob_name = f"request_images/{uuid.uuid4()}.{file.filename.split('.')[-1]}"
-        public_url = upload_to_gcs(file.file, BUCKET_NAME, destination_blob_name, file.content_type)
-        
-        db_image = crud.create_image(db=db, image=schemas.ImageCreate(filename=file.filename, url=public_url))
-        images.append(db_image)
-
-    db_request = crud.get_request(db=db, request_id=request_id)
-    if db_request is None:
-        raise HTTPException(status_code=404, detail="Request not found")
-
-    # Ensure db_request.imageURLs is a list and extend it with new IDs
-    current_image_ids = db_request.imageURLs or []
-    new_image_ids = [image.id for image in images]
-    current_image_ids.extend(new_image_ids)
-    db_request.imageURLs = current_image_ids
-
-    logging.info(f"Updated image IDs: {db_request.imageURLs}")
-
-    db.commit()
-    db.refresh(db_request)
-    
-    return images
-
 
 
 
